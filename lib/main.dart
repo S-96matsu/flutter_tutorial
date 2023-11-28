a1a5d80c6f66566048a041068bbcb8005111edad
S-96matsu <shiho.kuroyanagi@gmail.com>
2023-11-28 15:20:30 +0900

    topページの修正

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter layout demo',
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            color: Colors.blue,
+            width: 300,
+            height: 300,
+            child: Text('Hello World'),
+            alignment: Alignment.center,
+          ),
         ),
       ),
     );
